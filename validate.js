5d828a8aa9b9a08ab23a098633ab95be3ff710f5
edycyber <foodworldmeta@gmail.com>
2024-09-04 16:01:04 +0200

    Update validate.js

--- validate.js
+++ validate.js
@@ -4,7 +4,7 @@ function validate() {
     const validCodes = ['AI1532', 'AI1234', 'AI9876'];
 
     if (validCodes.includes(code)) {
-        window.location.href = 'https://edycyber.github.io/landingcreator/create.html';
+        window.location.href = 'client/app.html';
     } else {
         window.location.href = 'https://foodiverse0.gumroad.com/l/ppu';
     }
